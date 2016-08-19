91a8f1f3b52cd29ccec70d4df937e9a9d8b2d6ec
Jude Park <andmylife@naver.com>
2016-08-19 16:19:03 +0900

    Event Name Constants 들을 추가 중

---
+++ plating_android/app/src/main/java/com/plating/application/AnalyticsConstant.java
@@ -0,0 +1,20 @@
+package com.plating.application;
+
+/**
+ * Created by JudePark on 16. 8. 19..
+ */
+public class AnalyticsConstant {
+
+    public static final class Event {
+        public static String SPLASH = "splash";
+        public static String ENROLL_PROMOTION_CODE = "enroll_promotion_code";
+        public static String MY_COUPON = "my_coupon";
+        public static String EXIT_POINT = "exit_point";
+        public static String BACK_BUTTON_CLICK = "back_button_click";
+
+    }
+
+    public static final class Params {
+    }
+
+}
