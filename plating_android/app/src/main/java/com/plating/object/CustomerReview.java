a782da2d92e2890ae49cbbd3b4718b79bcbb21b0
Jude Park <andmylife@naver.com>
2016-08-24 14:43:13 +0900

    gradle property 수정

--- plating_android/app/src/main/java/com/plating/object/CustomerReview.java
+++ plating_android/app/src/main/java/com/plating/object/CustomerReview.java
@@ -1,8 +1,11 @@
 package com.plating.object;
 
+import lombok.Data;
+
 /**
  * Created by cheehoonha on 10/12/15.
  */
+@Data
 public class CustomerReview {
     public int idx;
     public double rating;
